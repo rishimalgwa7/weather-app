9e6046ada67d1924d2db947b72bc04ebfcd1f2b6
Rishi Malgwa <rishimalgwa35@gmail.com>
2021-05-07 20:34:25 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,75 @@
+const path = require("path");
+const express = require("express");
+const geocode = require('./utils/geocode');
+const forecast = require("./utils/forecast");
+
+const hbs = require("hbs");
+const app = express();
+
+const staticDirPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templets/views");
+const partialsPath = path.join(__dirname, "../templets/partials");
+
+app.set("view engine", "hbs");
+
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+app.use(express.static(staticDirPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "hello",
+    name: "rishi",
+  });
+});
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "help",
+    name: "rishi",
+    help: "this is help page",
+  });
+});
+app.get("/help/*", (req, res) => {
+  res.render("help404", {
+    title: "Help articl not found",
+  });
+});
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "Please provide City name",
+    });
+} else {
+    geocode(req.query.address, (error, {latitude,longitude,place}={}) => {
+        if (error) {
+          return res.send({
+            error
+          });
+        }
+      forecast(latitude,longitude,(error,forecastData)=>{
+        if (error) {
+          return res.send({
+            error
+          });
+         }
+         return res.send({
+           'forecast':forecastData,
+           address: req.query.address,
+           location:place
+
+        });
+        })
+    });
+    
+}
+
+});
+app.get("*", (req, res) => {
+  res.render("404page", {
+    title: "404 Error",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server started at port 3000");
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require("request");
+
+const forecast = (lat,long,callback)=>{
+    const url =
+    "http://api.weatherstack.com/current?access_key=a1543f0c3ed86febd82da0fee848a1df&query="+lat+","+long;
+  
+  request({ url,json:true }, (error, {body}) => {
+     if (error) {
+        callback("Unable to connect to service", undefined);
+     }else if(body.error){
+        callback("Wrong Cordinates provided", undefined);
+     } else {
+      const current =body.current;
+      callback(undefined,current.weather_descriptions[0]+" .Its feels like "+ current["feelslike"] +"but currently is"+current["temperature"]);
+     }
+  });
+  
+}
+
+module.exports = forecast;
+
