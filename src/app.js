be24d4e15d3d7eadb1037fe77674977a72abc169
Rishi Malgwa <rishimalgwa35@gmail.com>
2021-05-19 13:55:09 +0530

    heroku setup

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const geocode = require('./utils/geocode');
 const forecast = require("./utils/forecast");
-
+const port = process.env.PORT || 3000;
 const hbs = require("hbs");
 const app = express();
 
@@ -70,6 +70,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Server started at port 3000");
 });
